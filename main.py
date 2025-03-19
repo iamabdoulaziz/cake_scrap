57ce343e1555ee4f15e77ee76be7485881c7e8cb
Cute Boy <abdoulazizc867@gmail.com>
2025-03-19 15:29:29 +0000

    just extract description paragraph

--- main.py
+++ main.py
@@ -9,5 +9,8 @@ f.close()
 soup = BeautifulSoup(html_content, "html.parser")
 
 titre_h1 = soup.find("h1")
-print(titre_h1)
+
+paragraphe = soup.find("p", class_="description")
+print(titre_h1.text)
+print(f"Le pargraphe dans la description : {paragraphe.text}")
 
